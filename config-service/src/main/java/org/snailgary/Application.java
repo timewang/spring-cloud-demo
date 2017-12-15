46e73003877b49195dd15dfa358983bb77102b77
wangzhongfu <wangzhongfu@lakala.com>
2017-12-15 13:06:59 +0800

    配置中心 demo

--- config-service/src/main/java/org/snailgary/Application.java
+++ config-service/src/main/java/org/snailgary/Application.java
@@ -5,12 +5,13 @@ package org.snailgary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by wangzhongfu on 2017/12/15.
  */
+@EnableConfigServer
 @SpringBootApplication
-// @EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
