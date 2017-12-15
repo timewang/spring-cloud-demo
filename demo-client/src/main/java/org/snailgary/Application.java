6bf6dce4abfcc6b55b4083beec3c929150b24848
wangzhongfu <wangzhongfu@lakala.com>
2017-12-15 11:25:47 +0800

    spring cloud 初始化

---
+++ demo-client/src/main/java/org/snailgary/Application.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package org.snailgary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by wangzhongfu on 2017/12/15.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
