ea1f2bea9a7297d259b24816ab65905621ebde08
wangzhongfu <wangzhongfu@lakala.com>
2017-12-28 15:10:53 +0800

    同服务多实例部署

---
+++ demo-netflix2/src/main/java/org/snailgary/ComputeController.java
@@ -0,0 +1,32 @@
+package org.snailgary;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComputeController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping("/add")
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        //ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        //logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        client.getServices().forEach(id -> {
+            client.getInstances(id).forEach(instance -> {
+                logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+            });
+        });
+        return r;
+    }
+
+}
---
+++ demo-netflix2/src/main/java/org/snailgary/User.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package org.snailgary;
+
+/**
+ * Created by wangzhongfu on 2017/12/22.
+ */
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
